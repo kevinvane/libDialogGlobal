2dd2089832e2b80e6341a1eb805181e7eed4c70b
kevinwen <kevinwenwork@qq.com>
2022-08-19 06:05:09 +0000

    增加@JvmStatic注解，支持Java静态调用
    Signed-off-by: kevinwen <kevinwenwork@qq.com>

--- progresslibrary/src/main/java/com/gitee/kevinvane/dialog/DialogGlobal.kt
+++ progresslibrary/src/main/java/com/gitee/kevinvane/dialog/DialogGlobal.kt
@@ -8,11 +8,12 @@ class DialogGlobal {
     companion object{
 
         private var dialog: AlertDialog? = null
+        @JvmStatic
         fun show(context: Context?, message: String?){
             dialog = DialogProgress.wait(context, message)
             dialog?.show()
         }
-
+        @JvmStatic
         fun dismiss(){
             dialog?.dismiss()
         }
